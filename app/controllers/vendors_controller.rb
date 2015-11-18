3c74a6287c783e848b4c84566e81991e4085624d
Logan McDonald <loganmcdona11@gmail.com>
2015-11-18 14:37:20 -0800

    Added vendors table

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -3,7 +3,42 @@ class VendorsController < ApplicationController
   def index
   end
 
+  def find_vendor
+  	@vendor = Vendor.find(params[:id])
+  end
+
   def new 
   	@vendor = Vendor.new
   end
+
+  def show_vendors 
+  	@vendors = Vendor.all
+  end
+
+  def delete
+		@vendor = Vendor.destroy(params[:id])
+		redirect_to "/show_vendors_vendors"
+	end 
+
+	def create 
+		Vendor.create(vendor_params[:vendor_id])
+		redirect_to "/show_vendors_vendors"
+	end
+
+  def edit
+  	find_vendor
+  end
+
+  def update
+  	find_vendor
+
+  	@vendor.update(vendor_params[:vendor])
+  	redirect_to "/show_vendors_vendors"
+  end
+
+  private
+
+	def vendor_params
+		params.permit(vendor:[:name, :market_id])
+	end
 end
