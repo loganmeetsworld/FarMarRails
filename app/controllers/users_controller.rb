2f385ea4f0e04ba293fe2a4f9a915a355e6d575f
Tammy <misstamar@gmail.com>
2015-11-18 13:53:35 -0800

    moved the list into users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,16 @@ class UsersController < ApplicationController
 
 	def show
 	end
+
+	def markets
+		@markets = Market.all
+	end
+
+	def show_market
+	  @market = Market.find(params[:id])
+	end
+
+
+
+
 end
