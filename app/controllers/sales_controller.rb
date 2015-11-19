f5770d83eba9b909d3cb7a415c03287570c5e82e
Logan McDonald <loganmcdona11@gmail.com>
2015-11-19 15:18:30 -0800

    Added sales requirements

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -1,2 +1,35 @@
 class SalesController < ApplicationController
+	def find_vendor 
+		@vendor = Vendor.find(params[:id])
+	end
+
+  def find_product
+  	@product = Product.find(params[:product_id])
+  end
+
+  def find_sale
+  	@sale = Sale.find(params[:id])
+  end
+
+  def index
+  	find_vendor
+  	@sales = Sale.where(vendor_id: @vendor.id).order(:amount)
+  end
+
+	def new
+		@vendor = Vendor.find(params[:vendor_id])
+		find_product
+  	@sale = Sale.new
+  end
+
+  def create
+  	Sale.create(sale_params[:sale])
+		redirect_to vendor_products_path
+  end
+
+  private
+
+  def sale_params
+		params.permit(sale:[:amount, :purchase_time, :vendor_id, :product_id])
+	end
 end
