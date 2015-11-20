bc57d663ba18bd11f7397753a253a85cf5a19fb6
Logan McDonald <loganmcdona11@gmail.com>
2015-11-19 16:57:28 -0800

    Refractored sales

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,4 +1,15 @@
 class Sale < ActiveRecord::Base
 	belongs_to :vendor
 	belongs_to :market
+
+	def self.add_total(sales)
+		a = []
+		sales.each do |sale|
+			a.push(sale.amount)
+		end
+
+		return a.inject(:+).to_f/100
+	end
+
+
 end
